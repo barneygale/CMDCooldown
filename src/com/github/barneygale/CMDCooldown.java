3ef9d6b35dff8a333c33da6c5b8f745eee0f6cee
Barney Gale <barney.gale@gmail.com>
2013-03-03 01:03:30 +0000

    unfuck tabs.

--- src/com/github/barneygale/CMDCooldown.java
+++ src/com/github/barneygale/CMDCooldown.java
@@ -6,62 +6,62 @@ import java.util.HashMap;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class CMDCooldown extends JavaPlugin {
-	private CMDCooldownListener listener = new CMDCooldownListener(this);
-	
-	private HashMap<String, Integer> cache;    //player,command --> expiry time
-	private HashMap<String, Integer> commands; //command --> cooldown length
-	
-	public void onEnable() {
-		// get config
-	    File config_file = new File(getDataFolder(), "config.yml");
-	    if (!config_file.exists()) {
-	        getConfig().options().copyDefaults(true);
-	        saveConfig();
-	    }
-	    
-	    // get commands
-	    cache = new HashMap<String, Integer>();
+    private CMDCooldownListener listener = new CMDCooldownListener(this);
+    
+    private HashMap<String, Integer> cache;    //player,command --> expiry time
+    private HashMap<String, Integer> commands; //command --> cooldown length
+    
+    public void onEnable() {
+        // get config
+        File config_file = new File(getDataFolder(), "config.yml");
+        if (!config_file.exists()) {
+            getConfig().options().copyDefaults(true);
+            saveConfig();
+        }
+        
+        // get commands
+        cache = new HashMap<String, Integer>();
         commands = new HashMap<String, Integer>();
         for (String s : getConfig().getStringList("commands")) {
-        	try {
-        		String[] p = s.split(",");
-        		commands.put(p[0], Integer.parseInt(p[1]));
-        	}
-        	catch (Exception e) {
-        		getLogger().info("invalid config: "+s);
-        	}
+            try {
+                String[] p = s.split(",");
+                commands.put(p[0], Integer.parseInt(p[1]));
+            }
+            catch (Exception e) {
+                getLogger().info("invalid config: "+s);
+            }
         }
         
         //register listener
-	    getServer().getPluginManager().registerEvents(listener, this);
-	}
-	private int getTime() {
-		return (int) (System.currentTimeMillis() / 1000L);
-	}
-	private int getCooldown(String command) {
-    	for(String k : commands.keySet()) {
-    		if (command.startsWith(k)) {
-    			return commands.get(k).intValue();
-    		}
-    	}
-    	return 0;
+        getServer().getPluginManager().registerEvents(listener, this);
     }
-	public int getWait(String player, String command) {
-		int cooldown = getCooldown(command);
-		if (cooldown == 0) return 0;
-		
-		String key = player + ',' + command;
-		Integer t = cache.get(key);
-		if (t != null) {
-			int diff = t - getTime();
-			if (diff > 0) {
-				return diff;
-			} else {
-				cache.remove(key);
-			}
-		}
-		cache.put(key, getTime() + cooldown);
-		return 0;
-	}
-	
+    private int getTime() {
+        return (int) (System.currentTimeMillis() / 1000L);
+    }
+    private int getCooldown(String command) {
+        for(String k : commands.keySet()) {
+            if (command.startsWith(k)) {
+                return commands.get(k).intValue();
+            }
+        }
+        return 0;
+    }
+    public int getWait(String player, String command) {
+        int cooldown = getCooldown(command);
+        if (cooldown == 0) return 0;
+        
+        String key = player + ',' + command;
+        Integer t = cache.get(key);
+        if (t != null) {
+            int diff = t - getTime();
+            if (diff > 0) {
+                return diff;
+            } else {
+                cache.remove(key);
+            }
+        }
+        cache.put(key, getTime() + cooldown);
+        return 0;
+    }
+    
 }
--- src/com/github/barneygale/CMDCooldownListener.java
+++ src/com/github/barneygale/CMDCooldownListener.java
@@ -8,13 +8,13 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 
 public class CMDCooldownListener implements Listener {
-	private final CMDCooldown plugin;
+    private final CMDCooldown plugin;
     CMDCooldownListener(CMDCooldown instance) {
         plugin = instance;
     }
 
-	@EventHandler(priority = EventPriority.LOW)
-	private void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
+    @EventHandler(priority = EventPriority.LOW)
+    private void onPlayerCommandPreprocess(PlayerCommandPreprocessEvent event) {
         if (event.isCancelled()) return;
         
         Player player = event.getPlayer();
@@ -22,8 +22,8 @@ public class CMDCooldownListener implements Listener {
         
         int wait = plugin.getWait(player.getDisplayName(), event.getMessage());
         if (wait > 0) {
-        	player.sendMessage(ChatColor.RED + "[CMDCooldown] Please wait " + wait + " seconds!");
-        	event.setCancelled(true);
+            player.sendMessage(ChatColor.RED + "[CMDCooldown] Please wait " + wait + " seconds!");
+            event.setCancelled(true);
         }
     }
 }
